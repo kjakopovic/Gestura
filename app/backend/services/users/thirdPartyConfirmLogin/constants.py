6ea6b626a43cb385f57df47776e660db8a626878
dini777 <karlojakopov@gmail.com>
2025-03-29 20:15:07 +0100

    Added third party login

---
+++ app/backend/services/users/thirdPartyConfirmLogin/constants.py
@@ -0,0 +1,2 @@
+GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
+GOOGLE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v2/userinfo"
---
+++ app/backend/services/users/thirdPartyRequestLogin/constants.py
@@ -0,0 +1,4 @@
+GOOGLE_AUTHENTICATION_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+GOOGLE_SCOPE = "openid profile email"
+
+VALID_SERVICE_TYPES = ['google']
