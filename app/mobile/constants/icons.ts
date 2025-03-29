fddd2775568ba5f668685333b980ad177862fffd
Marin <75037869+Majki1@users.noreply.github.com>
2025-03-29 18:09:52 +0100

    [HOH-47] mobile login register (#2)
    * auth ui
    
    * login & register ui and validation

---
+++ app/mobile/constants/icons.ts
@@ -0,0 +1,10 @@
+import apple from "@/assets/icons/apple.png";
+import envelope from "@/assets/icons/envelope.png";
+import facebook from "@/assets/icons/facebook.png";
+import google from "@/assets/icons/google.png";
+import lock from "@/assets/icons/lock.png";
+import logoText from "@/assets/icons/logo-text.png";
+import logo from "@/assets/icons/logo.png";
+import person from "@/assets/icons/person.png";
+
+export { apple, envelope, facebook, google, lock, logoText, logo, person };
