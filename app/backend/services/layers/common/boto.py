3096f4428180b8a8f631b867fc57e5a3fc26a82a
dini777 <karlojakopov@gmail.com>
2025-04-05 18:27:20 +0200

    Redeploying users service

--- app/backend/services/layers/common/boto.py
+++ app/backend/services/layers/common/boto.py
@@ -7,34 +7,34 @@ logger = logging.getLogger("boto3")
 logger.setLevel(logging.INFO)
 
 _LAMBDA_USERS_TABLE_RESOURCE = {
-  "resource" : boto3.resource('dynamodb'),
-  "table_name" : environ.get("USERS_TABLE_NAME", "test_table")
+    "resource": boto3.resource("dynamodb"),
+    "table_name": environ.get("USERS_TABLE_NAME", "test_table"),
 }
 
+
 class LambdaDynamoDBClass:
-  """
-  AWS DynamoDB Resource Class
-  """
-  def __init__(self, lambda_dynamodb_resource):
     """
-    Initialize a DynamoDB Resource
+    AWS DynamoDB Resource Class
     """
-    self.resource = lambda_dynamodb_resource["resource"]
-    self.table_name = lambda_dynamodb_resource["table_name"]
-    self.table = self.resource.Table(self.table_name)
+
+    def __init__(self, lambda_dynamodb_resource):
+        """
+        Initialize a DynamoDB Resource
+        """
+        self.resource = lambda_dynamodb_resource["resource"]
+        self.table_name = lambda_dynamodb_resource["table_name"]
+        self.table = self.resource.Table(self.table_name)
+
 
 def get_secrets_from_aws_secrets_manager(secret_id, region_name):
-  try:
-    secrets_manager = boto3.client(
-      service_name='secretsmanager',
-      region_name=region_name
-    )
+    try:
+        secrets_manager = boto3.client(
+            service_name="secretsmanager", region_name=region_name
+        )
 
-    secret_string = secrets_manager.get_secret_value(
-      SecretId=secret_id
-    )
+        secret_string = secrets_manager.get_secret_value(SecretId=secret_id)
 
-    return json.loads(secret_string['SecretString'])
-  except Exception as e:
-    logger.error(f'Failed to retrieve secrets: {str(e)}')
-    return None
+        return json.loads(secret_string["SecretString"])
+    except Exception as e:
+        logger.error(f"Failed to retrieve secrets: {str(e)}")
+        return None
