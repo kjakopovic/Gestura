072194fd0923d120c31e37bfaf83fd42b6e47594
Marin <mikulec.marin52@gmail.com>
2025-04-12 17:34:11 +0200

    backend link & refactor round 1

--- app/mobile/components/CustomButton.tsx
+++ app/mobile/components/CustomButton.tsx
@@ -1,13 +1,20 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 type CustomButtonProps = {
   onPress: () => void;
   text: string;
   style?: string;
+  disabled?: boolean;
 };
 
-const CustomButton = ({ onPress, text, style }: CustomButtonProps) => {
+const CustomButton = ({
+  onPress,
+  text,
+  style,
+  disabled,
+  ...rest
+}: CustomButtonProps) => {
   return (
     <TouchableOpacity
       className={`w-3/4 self-center py-4 border ${
@@ -16,8 +23,12 @@ const CustomButton = ({ onPress, text, style }: CustomButtonProps) => {
           : style === "success"
           ? "border-success"
           : "border-error"
-      } bg-grayscale-700 rounded-2xl mt-16`}
+      } bg-grayscale-700 rounded-2xl mt-16 ${
+        disabled ? "opacity-50" : "opacity-100"
+      }`}
       onPress={onPress}
+      disabled={disabled}
+      {...rest}
     >
       <Text
         className={`${
---
+++ app/mobile/components/ResetPasswordSteps/index.ts
@@ -0,0 +1,3 @@
+export { default as EmailStep } from "./EmailStep";
+export { default as CodeStep } from "./CodeStep";
+export { default as PasswordStep } from "./PasswordStep";
