03929f22286f1253472dfa83fb616a735cca0dcb
NolanIle <nolanilisic@gmail.com>
2025-04-14 12:08:26 +0200

    Update schema

--- app/backend/services/learning/createTask/validation_schema.py
+++ app/backend/services/learning/createTask/validation_schema.py
@@ -2,22 +2,27 @@ schema = {
     "type": "object",
     "properties": {
         "section": {
-            "type": "number"
+            "type": "number",
+            "multipleOf": 10
         },
         "sectionName": {
             "type": "string"
         },
         "version": {
-            "type": "number"
+            "type": "number",
+            "enum": [1, 2, 3]
         },
         "question": {
             "type": "string"
         },
         "possibleAnswers": {
-            "type": "list"
+            "type": "array",
+            "minItems": 4,
+            "maxItems": 4,
         },
         "correctAnswerIndex": {
-            "type": "number"
+            "type": "number",
+            "enum": [0, 1, 2, 3]
         }
     },
     "required": ["section", "sectionName", "version",
