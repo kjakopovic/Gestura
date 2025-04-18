54d1fc008c549702c0843f2d783e39587b81363b
NolanIle <nolanilisic@gmail.com>
2025-04-18 11:05:15 +0200

    Update validation schema

--- app/backend/services/users/updateUserInfo/validation_schema.py
+++ app/backend/services/users/updateUserInfo/validation_schema.py
@@ -2,40 +2,44 @@ schema = {
   "type": "object",
   "properties": {
     "settings": {
-      "preferences": {
-        "type": "object",
-        "properties": {
-          "soundEffects": {"type": "boolean"},
-          "hapticFeedback": {"type": "boolean"},
+      "type": "object",
+      "properties": {
+        "preferences": {
+          "type": "object",
+          "properties": {
+            "soundEffects": {"type": "boolean"},
+            "hapticFeedback": {"type": "boolean"},
+          },
+          "additionalProperties": False
         },
-        "additionalProperties": False
+        "notifications": {
+          "type": "object",
+          "properties": {
+            "pushNotifications": {"type": "boolean"},
+            "heartRefill": {"type": "boolean"},
+            "dailyReminder": {"type": "boolean"},
+            "subscription": {"type": "boolean"},
+          },
+          "additionalProperties": False
+        },
+        "languageSettings": {
+          "type": "object",
+          "properties": {
+            "language": {"type": "string"},
+          },
+          "additionalProperties": False
+        },
+        "profile": {
+          "type": "object",
+          "properties": {
+            "username": {"type": "string"},
+            "email": {"type": "string", "format": "email"},
+            "phone": {"type": "string", "pattern": "^[0-9\\+\\-\\s\\(\\)]+$"},
+          },
+          "additionalProperties": False
+        }
       },
-      "notifications": {
-        "type": "object",
-        "properties": {
-          "pushNotifications": {"type": "boolean"},
-          "heartRefill": {"type": "boolean"},
-          "dailyReminder": {"type": "boolean"},
-          "subscription": {"type": "boolean"},
-        },
-        "additionalProperties": False
-      },
-      "languageSettings": {
-        "type": "object",
-        "properties": {
-          "language": {"type": "string"},
-        },
-        "additionalProperties": False
-      },
-      "profile": {
-        "type": "object",
-        "properties": {
-          "username": {"type": "string"},
-          "email": {"type": "string", "format": "email"},
-          "phone": {"type": "string", "pattern": "^[0-9\\+\\-\\s\\(\\)]+$"},
-        },
-        "additionalProperties": False
-      }
+      "additionalProperties": False
     }
   },
   "additionalProperties": False
