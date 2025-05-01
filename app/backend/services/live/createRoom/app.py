08cd235e38faf2daaeb0098b9a70d3f2a0a6ee72
kjakopovic <karlo.jakopov@gmail.com>
2025-05-01 20:07:55 +0200

    Adding logs to rooms

--- app/backend/services/live/createRoom/app.py
+++ app/backend/services/live/createRoom/app.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+import logging
 
 from boto import (
     LambdaDynamoDBClass,
@@ -9,24 +10,35 @@ from auth import get_email_from_jwt_token
 from middleware import middleware
 from common import build_response
 
+logger = logging.getLogger("CreateRoom")
+logger.setLevel(logging.DEBUG)
+
 
 @middleware
 def lambda_handler(event, context):
+    logger.debug(f"Event: {event}")
+
     jwt_token = event.get("headers").get("x-access-token")
     email = get_email_from_jwt_token(jwt_token)
 
     if not email:
+        logger.error("Invalid email in jwt token")
         return build_response(400, {"message": "Invalid email in jwt token"})
 
+    logger.debug(f"Email: {email}")
     global _LAMBDA_CHAT_ROOM_TABLE_RESOURCE, _LAMBDA_CONNECTIONS_TABLE_RESOURCE
     chatRoomDb = LambdaDynamoDBClass(_LAMBDA_CHAT_ROOM_TABLE_RESOURCE)
     connectionsDb = LambdaDynamoDBClass(_LAMBDA_CONNECTIONS_TABLE_RESOURCE)
 
     user = get_user_connection_by_email(connectionsDb, email)
     if not user or not user.get("connection_id"):
+        logger.error("User connection not found")
         return build_response(400, {"message": "User connection not found"})
 
+    logger.debug(f"User connection: {user}")
     room_id = str(uuid4())
+
+    logger.debug(f"Creating chat room with ID: {room_id}")
     chatRoomDb.put_item(Item={"chat_id": room_id, "users": [user.get("connection_id")]})
 
     return build_response(
--- app/backend/services/live/joinRoom/app.py
+++ app/backend/services/live/joinRoom/app.py
@@ -1,3 +1,5 @@
+import logging
+
 from boto import (
     LambdaDynamoDBClass,
     _LAMBDA_CHAT_ROOM_TABLE_RESOURCE,
@@ -7,27 +9,37 @@ from auth import get_email_from_jwt_token
 from middleware import middleware
 from common import build_response
 
+logger = logging.getLogger("CreateRoom")
+logger.setLevel(logging.DEBUG)
+
 
 @middleware
 def lambda_handler(event, context):
+    logger.debug(f"Event: {event}")
     room_id = event["pathParameters"]["roomId"]
 
+    logger.debug(f"Room ID: {room_id}")
     jwt_token = event.get("headers").get("x-access-token")
     email = get_email_from_jwt_token(jwt_token)
 
     if not email:
+        logger.error("Invalid email in jwt token")
         return build_response(400, {"message": "Invalid email in jwt token"})
 
+    logger.info("Setting up db connections")
     global _LAMBDA_CHAT_ROOM_TABLE_RESOURCE, _LAMBDA_CONNECTIONS_TABLE_RESOURCE
     chatRoomDb = LambdaDynamoDBClass(_LAMBDA_CHAT_ROOM_TABLE_RESOURCE)
     connectionsDb = LambdaDynamoDBClass(_LAMBDA_CONNECTIONS_TABLE_RESOURCE)
 
+    logger.debug("Getting user connection by email")
     user = get_user_connection_by_email(connectionsDb, email)
     if not user or not user.get("connection_id"):
         return build_response(400, {"message": "User connection not found"})
 
+    logger.debug(f"User connection: {user}")
     room = update_room(chatRoomDb, room_id, user.get("connection_id"))
     if not room:
+        logger.error("Room not found")
         return build_response(400, {"message": "Room not found"})
 
     return build_response(
