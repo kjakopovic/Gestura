346a3a529aed32160f544446eac3606988956bd3
NolanIle <nolanilisic@gmail.com>
2025-05-02 19:35:59 +0200

    Update register

--- app/backend/services/users/register/app.py
+++ app/backend/services/users/register/app.py
@@ -66,7 +66,7 @@ def sign_up_user(dynamodb, email, username, password):
         "hearts_next_refill": None,
         "letters_learned": {},
         "task_level": 0,
-        "current_level": 0,
+        "current_level": [],
         "time_played": 0,
         "xp": 0,
         "language_id": "usa",
